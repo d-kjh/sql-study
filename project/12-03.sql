7e2938c47b18c72e5344bd5f4e45365dc204c6a8
dkjh <dkjh9942@gmail.com>
2025-12-03 19:35:43 +0900

    12-03 event 및 trigger 중간 점검 및 index 작업 중

--- project/12-03.sql
+++ project/12-03.sql
@@ -1,7 +1,29 @@
 SELECT ss.*
 FROM screen_schedule ss
-JOIN screen sc
-    ON ss.screen_id = sc.screen_id
+         JOIN screen sc
+              ON ss.screen_id = sc.screen_id
 WHERE sc.theater_id = 38
-  AND ss.movie_id   = 41
-  AND ss.is_delete  = 0;
+  AND ss.movie_id = 41
+  AND ss.is_delete = 0;
+
+EXPLAIN
+SELECT reservation_id, status
+FROM reservation
+WHERE non_user_id IS NULL
+  AND user_id IS NULL;
+
+SELECT COUNT(1)
+FROM reservation
+WHERE user_id IS NOT NULL
+GROUP BY user_id;
+
+SELECT COUNT(1)
+FROM reservation
+WHERE non_user_id IS NOT NULL
+GROUP BY non_user_id;
+
+ALTER TABLE reservation
+ADD INDEX idx_nonuser_user (non_user_id, user_id);
+
+ALTER TABLE reservation
+DROP INDEX idx_nonuser_user;
