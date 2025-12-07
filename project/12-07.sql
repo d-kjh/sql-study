0c7172f2db39a665fea0f1e246af4a9a3a706c70
id <id@gmail.com>
2025-12-07 23:59:04 +0900

    12/07 index 추가중

--- project/12-07.sql
+++ project/12-07.sql
@@ -167,7 +167,10 @@ HAVING total_spent >= 300000
 ORDER BY total_spent DESC
 LIMIT 100;
 
+CREATE INDEX idx_res_vip_status_created_user_price
+ON reservation (status, created_at, user_id, price);
 
+DROP INDEX idx_res_vip_status_created_user_price ON reservation;
 
 SELECT m.movie_id,
        m.title,
@@ -193,10 +196,261 @@ SELECT
     m.title,
     COUNT(*)     AS audience_cnt
 FROM reservation r
-    FORCE INDEX (idx_res_status_schedule_price)   -- 🔥 이 인덱스 꼭 쓰게
+    FORCE INDEX (idx_res_status_schedule_price)
 JOIN screen_schedule ss ON ss.schedule_id = r.schedule_id
 JOIN movie m            ON m.movie_id    = ss.movie_id
 WHERE r.status = 1
 GROUP BY m.movie_id, m.title
 ORDER BY audience_cnt DESC;
 
+SELECT count(1) FROM reservation r
+WHERE non_user_id is NULL AND user_id IS NULL;
+
+EXPLAIN
+SELECT
+    pl.point_log_id,
+    pl.change_amount,
+    pl.status,          -- 0: 적립, 1: 사용, 2: 소멸
+    pl.created_at
+FROM point_log pl
+WHERE pl.user_id = 12
+  AND pl.created_at >= DATE_SUB(NOW(), INTERVAL 6 MONTH)
+ORDER BY pl.created_at DESC;
+
+EXPLAIN
+SELECT
+    u.user_id,
+    u.name,
+    u.email,
+    u.created_at
+FROM user u
+WHERE u.is_delete = 0
+  AND u.created_at < DATE_SUB(CURDATE(), INTERVAL 1 YEAR)
+  AND NOT EXISTS (
+      SELECT 1
+      FROM reservation r
+      WHERE r.user_id = u.user_id
+        AND r.created_at >= DATE_SUB(CURDATE(), INTERVAL 1 YEAR)
+  )
+  AND NOT EXISTS (
+      SELECT 1
+      FROM point_log pl
+      WHERE pl.user_id = u.user_id
+        AND pl.created_at >= DATE_SUB(CURDATE(), INTERVAL 1 YEAR)
+  );
+
+EXPLAIN
+SELECT
+    u.user_id,
+    u.name,
+    u.membership_id,
+    COUNT(*) AS available_coupon_cnt
+FROM user u
+JOIN coupon_detail cd
+  ON cd.user_id = u.user_id
+WHERE cd.status = 0                                  -- 미사용
+  AND cd.expired_date >= CURDATE()                  -- 아직 안 만료
+GROUP BY u.user_id, u.name, u.membership_id
+ORDER BY available_coupon_cnt DESC;
+
+
+CREATE INDEX idx_coupon_detail_status_expired_user
+    ON coupon_detail (status, expired_date, user_id);
+
+
+-- 관리자 대시보드: 최근 성공 결제 100건
+SELECT
+    p.payment_id,
+    p.payment_type,      -- 있으면
+    p.type_id,   -- 있으면
+    p.amount,
+    p.created_at
+FROM payment p
+WHERE p.status = 1              -- 성공 결제
+ORDER BY p.created_at DESC
+LIMIT 100;
+
+
+
+SELECT
+    r.reservation_id,
+    r.user_id,
+    r.schedule_id,
+    r.price,
+    r.created_at
+FROM reservation r
+WHERE r.status = 1
+  AND r.created_at >= '2025-11-20'
+  AND r.created_at <  '2025-11-27'
+ORDER BY r.created_at DESC;
+
+-- 관리자: 특정 등급 + 기간 조건으로 회원 목록 조회
+EXPLAIN
+SELECT
+    u.user_id,
+    u.name,
+    u.email,
+    u.membership_id,
+    u.created_at
+FROM user u
+WHERE u.is_delete = 0
+  AND u.membership_id IN (2, 3, 4)
+  AND u.created_at >= '2025-01-01'
+  AND u.created_at <  '2026-01-01'
+ORDER BY u.created_at DESC;
+
+CREATE INDEX idx_user_delete_member_created
+    ON user (is_delete, membership_id, created_at DESC);
+
+
+SELECT
+    u.user_id,
+    u.name,
+    u.email,
+    u.created_at
+FROM user u
+WHERE u.is_delete = 0
+  AND u.created_at < DATE_SUB(CURDATE(), INTERVAL 6 MONTH)
+  AND NOT EXISTS (
+      SELECT 1
+      FROM reservation r
+      WHERE r.user_id = u.user_id
+  )
+ORDER BY u.created_at
+LIMIT 100;
+
+SELECT
+    u.user_id,
+    u.name,
+    u.membership_id,
+
+    r.reservation_id,
+    r.status              AS reservation_status,
+    r.price               AS reservation_price,
+    r.created_at          AS reservation_at,
+
+    ss.running_date,
+    ss.start_time,
+
+    t.name                AS theater_name,
+    s.name                AS screen_name,
+    m.title               AS movie_title,
+
+    p.payment_id,
+    p.amount              AS payment_amount,
+    p.created_at          AS payment_at
+
+FROM user u
+JOIN reservation r
+  ON r.user_id = u.user_id
+JOIN screen_schedule ss
+  ON ss.schedule_id = r.schedule_id
+JOIN screen s
+  ON s.screen_id = ss.screen_id
+JOIN theater t
+  ON t.theater_id = s.theater_id
+JOIN movie m
+  ON m.movie_id = ss.movie_id
+LEFT JOIN payment p
+  ON p.payment_type = 0
+ AND p.type_id      = r.reservation_id
+ AND p.status       = 1
+
+WHERE u.user_id = 12
+  AND r.status = 1
+  AND r.created_at >= DATE_SUB(CURDATE(), INTERVAL 3 MONTH)
+
+ORDER BY r.created_at DESC
+LIMIT 50;
+
+CREATE INDEX idx_res_user_status_created
+    ON reservation (user_id, status, created_at DESC);
+CREATE INDEX idx_pay_type_target_status_created
+    ON payment (payment_type, type_id, status, created_at);
+
+DROP INDEX idx_res_user_status_created on reservation;
+DROP INDEX idx_pay_type_target_status_created on payment;
+
+-- 최근 1개월 동안 영화 + 스토어 결제 합산 기준 TOP 100 유저
+SELECT
+    u.user_id,
+    u.name,
+    SUM(x.paid_amount) AS total_spent
+FROM (
+    -- 1) 영화 예매 결제
+    SELECT
+        r.user_id,
+        p.amount AS paid_amount
+    FROM payment p FORCE INDEX (idx_pay_type_status_created_target)
+    JOIN reservation r
+      ON p.payment_type = 0              -- 영화 예매 결제
+     AND p.type_id      = r.reservation_id
+    WHERE p.status = 1                    -- 결제 성공
+      AND p.created_at >= DATE_SUB(CURDATE(), INTERVAL 1 MONTH)
+
+    UNION ALL
+        -- 2) 스토어 결제
+    SELECT
+        o.user_id,
+        p.amount AS paid_amount
+    FROM payment p FORCE INDEX (idx_pay_type_status_created_target)
+    JOIN `order` o
+      ON p.payment_type = 1              -- 스토어 결제
+     AND p.type_id      = o.order_id
+    WHERE p.status = 1
+      AND p.created_at >= DATE_SUB(CURDATE(), INTERVAL 1 MONTH)
+) x
+JOIN user u
+  ON u.user_id = x.user_id
+GROUP BY u.user_id, u.name
+ORDER BY total_spent DESC
+LIMIT 100;
+
+CREATE INDEX idx_pay_type_status_created_target
+    ON payment (payment_type, status, created_at, type_id, amount);
+
+DROP INDEX idx_pay_type_status_created_target on payment;
+
+-- 최근 1개월, 영화 예매 결제 기준 TOP 100 유저
+SELECT
+    u.user_id,
+    u.name,
+    SUM(p.amount) AS total_movie_spent
+FROM payment p FORCE INDEX (idx_pay_type_status_created_target)
+JOIN reservation r
+  ON p.payment_type = 0              -- 영화 예매 결제
+ AND p.type_id      = r.reservation_id
+JOIN user u
+  ON u.user_id = r.user_id
+WHERE p.status = 1                    -- 결제 성공
+  AND p.created_at >= DATE_SUB(CURDATE(), INTERVAL 1 MONTH)
+GROUP BY u.user_id, u.name
+ORDER BY total_movie_spent DESC
+LIMIT 100;
+
+
+-- 최근 1개월, 스토어 결제 기준 TOP 100 유저
+SELECT
+    u.user_id,
+    u.name,
+    SUM(p.amount) AS total_store_spent
+FROM payment p
+JOIN `order` o
+  ON p.payment_type = 1              -- 스토어 결제
+ AND p.type_id      = o.order_id
+JOIN user u
+  ON u.user_id = o.user_id
+WHERE p.status = 1                    -- 결제 성공
+  AND p.created_at >= DATE_SUB(CURDATE(), INTERVAL 1 MONTH)
+GROUP BY u.user_id, u.name
+ORDER BY total_store_spent DESC
+LIMIT 100;
+
+CREATE INDEX idx_pay_type_status_created_target
+    ON payment (
+        payment_type,   -- 0(영화) / 1(스토어)
+        status,         -- 1: 성공
+        created_at,     -- 최근 1개월 범위
+        type_id,        -- reservation_id 또는 order_id
+        amount          -- SUM(amount) 커버링용
+    );
